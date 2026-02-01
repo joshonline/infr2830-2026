8eff2e832bd5af4605c3b0ae08ed4f3ca7dab5f8
joshonline <masanzajoshua@gmail.com>
2026-01-31 22:24:55 -0500

    str to int conversion

--- assignments/assignment1/exercise9_geo_seq.c
+++ assignments/assignment1/exercise9_geo_seq.c
@@ -8,14 +8,25 @@ int main(int argc, char *argv[]){
     if (argc !=4){ //check argument count, argc
         fprintf(stderr, "Usage: %s n a r\n", argv[0]);
         return 1; //end with error
-    } 
-    // array: empty array, holds sequence
-    // k=0 //count sequence 
-    // 
+    };
+
+    // string to int conversion
+    int n = atoi(argv[1]);
+    int a = atoi(argv[2]);
+    int r = atoi(argv[3]);
+
+    if (n < 0 || a < 0 || r < 0){
+        fprintf(stderr, "All arguments must be integers\n");
+        return 1;
+    }
+    else{
+        printf("Argumets: n=%d, a=%d, r=%d", n, a, r);
+    }
+
     // for k=0, while k<n, k++
         // print a*(r**k)
-        // 
-    // 
+        //
+    //
     // retturn 0
     return 0;
 }
