dbed98616cd2d2cecf4bf94d642275374a3a4dd7
joshonline <masanzajoshua@gmail.com>
2026-01-31 22:08:16 -0500

    libraries and error checking

--- assignments/assignment1/exercise9_geo_seq.c
+++ assignments/assignment1/exercise9_geo_seq.c
@@ -1,8 +1,14 @@
 //import headers
-// 
+#include <stdio.h>
+#include <stdlib.h>
+
 // main(int n, int a, int r)
+int main(int argc, char *argv[]){
     // error check: args passed must be int
-    // 
+    if (argc !=4){ //check argument count, argc
+        fprintf(stderr, "Usage: %s n a r\n", argv[0]);
+        return 1; //end with error
+    } 
     // array: empty array, holds sequence
     // k=0 //count sequence 
     // 
@@ -11,3 +17,5 @@
         // 
     // 
     // retturn 0
+    return 0;
+}
