ee0a48757e86f7dc63c3d4e5343546b4e2d9ec02
joshonline <masanzajoshua@gmail.com>
2026-01-31 23:00:06 -0500

    exercise 9 complete attempt 2 - child process implementation

--- assignments/assignment1/exercise9_geo_seq.c
+++ assignments/assignment1/exercise9_geo_seq.c
@@ -1,6 +1,9 @@
 //link headers
 #include <stdio.h>
 #include <stdlib.h>
+#include <sys/types.h>
+#include <sys/wait.h>
+#include <unistd.h>
 
 // main(int n, int a, int r)
 int main(int argc, char *argv[]){
@@ -23,14 +26,24 @@ int main(int argc, char *argv[]){
         printf("Argumets: n=%d, a=%d, r=%d\n", n, a, r);
     }
 
-    int term = a;
-    printf("Sequence:");
-    // for k=0, while k<n, k++
-    for(int k=0; k<n; k++){
-        // print current term
-        printf("%d, ",term);
-        // multiply current term by r
-        term *= r;
+    pid_t pid = fork();
+
+    if (pid<0){
+        fprintf(stderr, "Fork failed\n");
+        return 1;
+    } else if(pid == 0){
+        int term = a;
+        printf("Sequence:");
+        // for k=0, while k<n, k++
+        for(int k=0; k<n; k++){
+            // print current term
+            printf("%d, ",term);
+            // multiply current term by r
+            term *= r;
+        }
+        exit(0);
+    } else{
+        wait(NULL);
     }
 
     // retturn 0
