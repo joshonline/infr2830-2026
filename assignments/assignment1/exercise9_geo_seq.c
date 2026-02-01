94297f43eb238398628e319d90700783822edc38
joshonline <masanzajoshua@gmail.com>
2026-01-31 21:50:08 -0500

    pseudocode for exercise 9

---
+++ assignments/assignment1/exercise9_geo_seq.c
@@ -0,0 +1,16 @@
+//import headers
+// 
+// main(int n, int a, int r)
+    // error check: args passed must be int
+    // 
+    // array: empty array, holds sequence
+    // k=0 //count sequence 
+    // 
+    // for k=0, while k<n, k++
+        // add a*(r**k) to array
+    // 
+    // string output = "" //empty string
+    // concatenate arrey to output
+    // printf(output)
+    // 
+    // retturn 0
